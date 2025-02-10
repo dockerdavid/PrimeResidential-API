1ab59c4174758cb2233828d81852ea2d78007792
dockerdavid <jdgomezdelgado@uts.edu.co>
2025-02-09 23:24:54 -0500

    feat: add ExtrasByServiceEntity to ServicesModule imports for enhanced functionality

--- src/api/services/services.module.ts
+++ src/api/services/services.module.ts
@@ -4,11 +4,12 @@ import { Module } from '@nestjs/common';
 import { ServicesController } from './services.controller';
 import { ServicesService } from './services.service';
 
+import { ExtrasByServiceEntity } from 'src/entities/extras_by_service.entity';
 import { ServicesEntity } from 'src/entities/services.entity';
 
 @Module({
   controllers: [ServicesController],
   providers: [ServicesService],
-  imports: [TypeOrmModule.forFeature([ServicesEntity])],
+  imports: [TypeOrmModule.forFeature([ServicesEntity, ExtrasByServiceEntity])],
 })
 export class ServicesModule { }
