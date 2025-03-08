b36b8d5fad27e0270744814d5e292a84f7917b02
dockerdavid <jdgomezdelgado@uts.edu.co>
2025-03-08 12:00:09 -0500

    feat: update UpdateUserDto to omit password field from CreateUserDto for enhanced security

--- src/api/users/dto/update-user.dto.ts
+++ src/api/users/dto/update-user.dto.ts
@@ -1,12 +1,7 @@
-import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { PartialType, OmitType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
-import { IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-    @ApiProperty({
-        description: 'The id of the user',
-        example: "1",
-    })
-    @IsString()
-    id: string
+export class UpdateUserDto extends PartialType(
+    OmitType(CreateUserDto, ['password'] as const),
+) {
 }
