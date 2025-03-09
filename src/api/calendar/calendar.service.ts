a97cf41a3c8a19e618897c2de6f31a295ec9f71c
dockerdavid <jdgomezdelgado@uts.edu.co>
2025-03-08 19:01:32 -0500

    feat: refactor findOne method in CalendarService to streamline date-based querying logic

--- src/api/calendar/calendar.service.ts
+++ src/api/calendar/calendar.service.ts
@@ -1,9 +1,7 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { Injectable } from '@nestjs/common';
-
-import { Repository, Between, MoreThanOrEqual, LessThanOrEqual } from 'typeorm';
+import { Repository, Between } from 'typeorm';
 import * as moment from 'moment';
-
 import { ServicesEntity } from 'src/entities/services.entity';
 
 @Injectable()
@@ -11,48 +9,43 @@ export class CalendarService {
   constructor(
     @InjectRepository(ServicesEntity)
     private readonly servicesRepository: Repository<ServicesEntity>,
-  ) { }
+  ) {}
 
   async findOne(id: string) {
     const date = moment().format('YYYY-MM-DD');
 
+    let whereCondition: any;
+
     if (id === 'day') {
-      return this.servicesRepository.find({ where: { date } });
+      whereCondition = { date };
+    } else if (id === 'week') {
+      whereCondition = {
+        date: Between(
+          moment().startOf('isoWeek').format('YYYY-MM-DD'),
+          moment().endOf('isoWeek').format('YYYY-MM-DD')
+        ),
+      };
+    } else if (id === 'month') {
+      whereCondition = {
+        date: Between(
+          moment().startOf('month').format('YYYY-MM-DD'),
+          moment().endOf('month').format('YYYY-MM-DD')
+        ),
+      };
+    } else if (id === 'year') {
+      whereCondition = {
+        date: Between(
+          moment().startOf('year').format('YYYY-MM-DD'),
+          moment().endOf('year').format('YYYY-MM-DD')
+        ),
+      };
+    } else {
+      return [];
     }
 
-    if (id === 'week') {
-      const startOfWeek = moment().startOf('isoWeek').format('YYYY-MM-DD');
-      const endOfWeek = moment().endOf('isoWeek').format('YYYY-MM-DD');
-
-      return this.servicesRepository.find({
-        where: {
-          date: Between(startOfWeek, endOfWeek),
-        },
-      });
-    }
-
-    if (id === 'month') {
-      const startOfMonth = moment().startOf('month').format('YYYY-MM-DD');
-      const endOfMonth = moment().endOf('month').format('YYYY-MM-DD');
-
-      return this.servicesRepository.find({
-        where: {
-          date: Between(startOfMonth, endOfMonth),
-        },
-      });
-    }
-
-    if (id === 'year') {
-      const startOfYear = moment().startOf('year').format('YYYY-MM-DD');
-      const endOfYear = moment().endOf('year').format('YYYY-MM-DD');
-
-      return this.servicesRepository.find({
-        where: {
-          date: Between(startOfYear, endOfYear),
-        },
-      });
-    }
-
-    return [];
+    return this.servicesRepository.find({
+      where: whereCondition,
+      relations: ['community', 'type', 'status'],
+    });
   }
 }
