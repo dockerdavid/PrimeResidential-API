9597cad5c5da0483594cda713ebf76dedae60a59
dockerdavid <jdgomezdelgado@uts.edu.co>
2025-03-16 19:16:12 -0500

    feat: add new report endpoints for general, cleaner, and invoice reports

--- src/api/reports/reports.controller.ts
+++ src/api/reports/reports.controller.ts
@@ -8,6 +8,30 @@ import { ReportsService } from './reports.service';
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) { }
 
+  @Get('/reporte-general/:date')
+  reporteGeneral(@Res() response: Response, @Param('date', new ParseDatePipe()) date: string) {
+    const pdfDoc = this.reportsService.reporteSemana(date);
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('/reporte-cleaner/:date')
+  reporteCleaner(@Res() response: Response, @Param('date', new ParseDatePipe()) date: string) {
+    const pdfDoc = this.reportsService.reporteSemana(date);
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
+  @Get('/reporte-invoce/:date')
+  reporteInvoice(@Res() response: Response, @Param('date', new ParseDatePipe()) date: string) {
+    const pdfDoc = this.reportsService.reporteSemana(date);
+    response.setHeader('Content-Type', 'application/pdf');
+    pdfDoc.pipe(response);
+    pdfDoc.end();
+  }
+
   @Get('/reporte-semana/:date')
   reporteSemana(@Res() response: Response, @Param('date', new ParseDatePipe()) date: string) {
     const pdfDoc = this.reportsService.reporteSemana(date);
