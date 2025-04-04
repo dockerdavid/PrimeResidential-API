069055200998f7463b9aab5a4a40cf724bdb309d
dockerdavid <jdgomezdelgado@uts.edu.co>
2025-04-04 17:10:28 -0500

    feat: add PushNotificationsService to ServicesModule providers

--- src/api/services/services.module.ts
+++ src/api/services/services.module.ts
@@ -6,10 +6,11 @@ import { ServicesService } from './services.service';
 import { ServicesEntity } from '../../entities/services.entity';
 import { ExtrasByServiceEntity } from '../../entities/extras_by_service.entity';
 import { UsersEntity } from '../../entities/users.entity';
+import { PushNotificationsService } from '../../push-notification/push-notification.service';
 
 @Module({
   controllers: [ServicesController],
-  providers: [ServicesService],
+  providers: [ServicesService, PushNotificationsService],
   imports: [TypeOrmModule.forFeature([ServicesEntity, ExtrasByServiceEntity, UsersEntity])],
 })
 export class ServicesModule { }
